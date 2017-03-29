ff8af410f9da86ed58e752c0e633d3e330f070b3
Felipe Carvalho <felipe.carvalho@gmail.com>
2017-03-29 23:13:10 +1100

    barebones initial version, allows the creation and retrieval of places to have lunch

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const collectionName = 'places';
+const root = '/';
+const routerRoot = root + collectionName;
+const mongoOptions = {};
+
+var bodyParser = require('body-parser');
+var router = express.Router();
+var mongoose   = require('mongoose');
+var Place = require('./app/model/Place');
+var argv = require('yargs').argv;
+
+const mongoUri = argv.mongoUri;
+const port = argv.port || 3000;
+
+mongoose.connect(mongoUri, mongoOptions).then(
+  () => {
+    app.listen(port, function() {
+      console.log('listening on port ' + port);
+    });
+  },
+  err => {
+    console.log(err);
+  }
+);
+
+app.use(bodyParser.json());
+app.use(routerRoot, router);
+
+router.get(root, (req, res) => {
+  Place.find((err, places) => {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    }
+
+    res.json(places);
+  });
+});
+
+router.post(root, (req, res) => {
+  var newPlace = new Place();
+
+  newPlace.name = req.body.name;
+  newPlace.save(err => {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    }
+
+    console.log(newPlace.name + ' saved to database');
+    res.sendStatus(201);
+  });
+});
