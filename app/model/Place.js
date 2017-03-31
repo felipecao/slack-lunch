6fde52f7a5379a85b00d138114e98eff702b9695
Felipe Carvalho <felipe.carvalho@gmail.com>
2017-03-31 22:48:17 +1100

    improved error handling and reporting

--- app/model/Place.js
+++ app/model/Place.js
@@ -4,7 +4,8 @@ var Schema = mongoose.Schema;
 var PlaceSchema = new Schema({
     name: {
       type: String,
-      required: true
+      required: true,
+      unique: true
     }
 });
 
--- server.js
+++ server.js
@@ -28,6 +28,24 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
 app.use(routerRoot, router);
 
+function sendResponse(res, text, status = 200) {
+  return res.status(status).send({
+    esponse_type: "in_channel",
+    text: text
+  });
+}
+
+function sendError(res, err) {
+  console.log(err);
+
+  if (err.code === 11000) {
+    console.log('constraint violation');
+    return res.status(409).send('Place already exists');
+  }
+
+  return res.status(400).send(err);
+}
+
 router.post('/add', (req, res) => {
   var newPlace = new Place();
 
@@ -38,28 +56,34 @@ router.post('/add', (req, res) => {
 
   newPlace.save(err => {
     if (err) {
-      console.log(err);
-      res.send(err);
+      return sendError(res, err);
     }
 
-    console.log(newPlace.name + ' saved to database');
-    res.status(201).send(`@${req.body.user_name} your new place ${newPlace.name} has been added!`);
+    console.log(`'${newPlace.name}' saved to database`);
+
+    return sendResponse(
+      res,
+      `@${req.body.user_name} your new place ${newPlace.name} has been added!`,
+      201
+    );
   });
 });
 
 router.post('/show', (req, res) => {
   Place.find((err, places) => {
     if (err) {
-      console.log(err);
-      res.send(err);
+      return sendError(res, err);
     }
 
     if (!places.length) {
-      res.status(200).send({response_type: "in_channel", text: `@${req.body.user_name} there are no places yet! Why don't you try to create the first one by using the \`/add\` command?`});
+      return sendResponse(
+        res,
+        `@${req.body.user_name} there are no places yet! Why don't you try to create the first one by using the \`/add\` command?`
+      );
     }
 
-    let names = places.map(p => p.name).join(',');
+    const names = places.map(p => `*${p.name}*`).join(',');
 
-    res.status(200).send({response_type: "in_channel", text: `@${req.body.user_name} these are the places in our database: ${names}`});
+    return sendResponse(res, `@${req.body.user_name} these are the places in our database: ${names}`);
   });
 });
