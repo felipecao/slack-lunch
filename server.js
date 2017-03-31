19f4712432ed671e16b15117a0f02c58325c4151
Felipe Carvalho <felipe.carvalho@gmail.com>
2017-03-31 23:38:09 +1100

    fixed display of commands names under /menu command

--- server.js
+++ server.js
@@ -103,8 +103,8 @@ router.post('/random', (req, res) => {
 
 router.post('/menu', (req, res) => {
   return sendResponse(res, `@${req.body.user_name} these are the commands we have available:
-\`add\`: add a new place to have lunch
-\`show\`: shows all places saved to our database
-\`random\`: picks a random place for you to have lunch
-\`menu\`: displays this message`);
+\`/add\`: add a new place to have lunch
+\`/show\`: shows all places saved to our database
+\`/random\`: picks a random place for you to have lunch
+\`/menu\`: displays this message`);
 });
