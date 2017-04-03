01e176db4cce9796cf10db27adcc1ea35420daab
Felipe Carvalho <felipe.carvalho@gmail.com>
2017-04-03 15:50:58 +1000

    add first passing test

--- test/router/menu.spec.js
+++ test/router/menu.spec.js
@@ -1,19 +1,21 @@
+var proxyquire = require('proxyquire').noCallThru();
+
 var sinon = require('sinon');
 var expect = require('chai').expect;
 var assert = require('assert');
-var sendResponse = require('../../app/router/sendResponse');
-var subject = require('../../app/router/menu');
+
+const sendResponseStub = sinon.stub();
+const menu = proxyquire('../../app/router/menu', {
+  './sendResponse': sendResponseStub
+});
 
 describe('Menu', function() {
   it('should print information about the available commands', function() {
 
-    var req = sinon.stub();
+    var req = {body: {user_name: 'felipe'}};
     var res = sinon.stub();
-    var spy = sinon.spy(sendResponse, "sendResponse");
 
-    req.body.user_name = 'felipe';
-
-    subject.menu(req, res);
+    menu(req, res);
 
     expect(sendResponse).to.have.been.called.with(res, 'felipe');
 
---
+++ test/router/sendResponse.spec.js
@@ -0,0 +1,31 @@
+var sinon = require('sinon');
+var assert = require('assert');
+var sendResponse = require('../../app/router/sendResponse');
+
+const successStatus = 200;
+const responseContract = {
+  status: function(number) {
+
+  },
+  send: function(json) {
+
+  }
+};
+
+describe('sendResponse', function() {
+  it('should send a JSON object over the response ', function() {
+    const notificationText = "notification text";
+
+    var statusStub = sinon.stub(responseContract, 'status');
+    var sendStub = sinon.stub(responseContract, 'send');
+
+    statusStub.withArgs(successStatus).returns(responseContract);
+
+    sendResponse(responseContract, notificationText);
+
+    assert(sendStub.calledWith({
+      response_type: "in_channel",
+      text: notificationText
+    }));
+  });
+});
